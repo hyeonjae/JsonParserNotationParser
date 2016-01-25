3c2e55d84de8c380f824cf1781106178cd10313d
박현재/NE <hyeonjae.park@nhnent.com>
2016-01-25 15:44:43 +0900

    add testcase

--- src/test/java/JsonNodeParserTest.java
+++ src/test/java/JsonNodeParserTest.java
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -39,6 +40,53 @@ public class JsonNodeParserTest {
         Assert.assertEquals("red", parser.get("aaa.lll[*][*].color").get(0));
         Assert.assertEquals("blue", parser.get("aaa.lll[*][*].color").get(1));
         Assert.assertEquals("green", parser.get("aaa.lll[*][*].color").get(2));
+    }
+
+    @Test
+    public void testBasic() throws Exception {
+        String payload = "{\"friends\":[{\"id\":0,\"name\":\"Santiago Mcmillan\"},{\"id\":1,\"name\":\"Graham Zimmerman\"},{\"id\":2,\"name\":\"Tabatha Garrison\"}]}";
+        JsonNodeParser parser = new JsonNodeParser(mapper.readValue(payload, JsonNode.class));
+
+        Assert.assertEquals(1, parser.get("friends[0].id").size());
+        Assert.assertEquals(1, parser.get("friends[1].id").size());
+        Assert.assertEquals(1, parser.get("friends[2].id").size());
+
+        Assert.assertEquals("0", parser.get("friends[0].id").get(0));
+        Assert.assertEquals("1", parser.get("friends[1].id").get(0));
+        Assert.assertEquals("2", parser.get("friends[2].id").get(0));
+
+        Assert.assertEquals(1, parser.get("friends[0].name").size());
+        Assert.assertEquals(1, parser.get("friends[1].name").size());
+        Assert.assertEquals(1, parser.get("friends[2].name").size());
+
+        Assert.assertEquals("Santiago Mcmillan", parser.get("friends[0].name").get(0));
+        Assert.assertEquals("Graham Zimmerman", parser.get("friends[1].name").get(0));
+        Assert.assertEquals("Tabatha Garrison", parser.get("friends[2].name").get(0));
+
+        Assert.assertEquals(3, parser.get("friends[*].id").size());
+        Assert.assertEquals(3, parser.get("friends[*].name").size());
+
+        Assert.assertEquals("Santiago Mcmillan", parser.get("friends[*].name").get(0));
+        Assert.assertEquals("Graham Zimmerman", parser.get("friends[*].name").get(1));
+        Assert.assertEquals("Tabatha Garrison", parser.get("friends[*].name").get(2));
+
+
+        Assert.assertEquals(0, parser.get("friends[0].xxx").size());
+        Assert.assertEquals(0, parser.get("friends[3].name").size());
+        Assert.assertEquals(0, parser.get("xxx").size());
+    }
+
+    // TODO
+    @Test
+    @Ignore
+    public void test배열() throws Exception {
+        String payload = "[{\"aaa\":\"hello\"}, {\"aaa\":\"world\"}]";
+        JsonNodeParser parser = new JsonNodeParser(mapper.readValue(payload, JsonNode.class));
+
+        Assert.assertEquals("hello", parser.get("[0].aaa").get(0));
+        Assert.assertEquals("world", parser.get("[1].aaa").get(0));
+        Assert.assertEquals("world", parser.get("[*].aaa").get(0));
+        Assert.assertEquals("world", parser.get("[*].aaa").get(1));
     }
 
     @Test
